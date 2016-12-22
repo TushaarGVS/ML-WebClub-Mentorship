7e109f176753a29af608d0a49d7327a15ec3a24b
Tushaar Gangarapu <tushaargvsg45@gmail.com>
2016-12-22 09:16:42 +0530

    Update SVM_Programs.py

--- SVM_Programs.py
+++ SVM_Programs.py
@@ -1,5 +1,23 @@
 # Support Vector Machines with Python
 
+# Basic Outline:
+
+import numpy
+import matplotlib.pyplot as plt
+from sklearn import svm
+from sklearn import datasets
+
+# Assume that you have, x (predictor) and y (target) for training data set and x_test(predictor) of test_dataset
+
+clf = svm.SVC(kernel = "linear", C = 1, gamma = 1)
+# Create SVM classification object, C = SVM Regularization Parameter
+
+# Next, train the model using training sets and check score
+clf.fit(x, y)
+clf.score(x, y)
+
+prediction = clf.predict(x_test)
+
 # Program - 01: Image Recognition
 
 import matplotlib.pyplot as plt
@@ -40,3 +58,36 @@ plt.show()
 '''
 
 # change gamma = 0.001 to gamma = 0.01 and check out the changes
+
+# Program - 02:
+
+import numpy
+import matplotlib.pyplot as plt
+from sklearn import svm, datasets
+
+iris = datasets.load_iris() # load some data for manipulation
+x = iris.data[:, :2] # we take only first two features
+y = iris.target
+
+clf = svm.SVC(kernel = "linear", C = 1, gamma = 0).fit(x, y)
+
+# create a mesh to plot
+x_min, x_max = x[:, 0]. min() - 1, x[:, 0].max() + 1
+y_min, y_max = y[:, 0]. min() - 1, y[:, 0].max() + 1
+h = (x_max / x_min) / 100
+xx, yy = numpy.meshgrid(np.arrange(x_min, x_max, h), np.arrange(y_min, y_max, h))
+
+plt.subplot(1, 1, 1)
+
+Z = svc.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+plt.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.8)
+
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Paired)
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+plt.xlim(xx.min(), xx.max())
+plt.title('SVC with linear kernel')
+plt.show()
+
+# Reference: http://scikit-learn.org/stable/tutorial/basic/tutorial.html
